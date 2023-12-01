cbff28d48491c5beae7f89b4d7866b123f6d0c07
Mickael Titeca <mickael.titeca@gmail.com>
2023-12-01 10:45:08 +0100

    chore: Project init

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# aoc-2023
-Advent of Code 2023
+A sample command-line application with an entrypoint in `bin/`, library code
+in `lib/`, and example unit test in `test/`.
---
+++ test/aoc_2023_test.dart
@@ -0,0 +1,8 @@
+import 'package:aoc_2023/aoc_2023.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
