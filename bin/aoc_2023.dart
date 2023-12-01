455ce4679a68f9b2cae10d045a7a983234b59be9
Mickael Titeca <mickael.titeca@gmail.com>
2023-12-01 11:29:53 +0100

    Day 1 part 1 solution

--- bin/aoc_2023.dart
+++
@@ -1,5 +0,0 @@
-import 'package:aoc_2023/aoc_2023.dart' as aoc_2023;
-
-void main(List<String> arguments) {
-  print('Hello world: ${aoc_2023.calculate()}!');
-}
--- lib/aoc_2023.dart
+++ lib/aoc_2023.dart
@@ -1,3 +1,24 @@
-int calculate() {
-  return 6 * 7;
+int textCalibrationValue(String text) {
+  final regExp = RegExp(r'\d');
+  final matches = regExp.allMatches(text);
+  final firstMatch = matches.firstOrNull;
+  final lastMatch = matches.lastOrNull;
+
+  int intValue(RegExpMatch? match) {
+    return match == null
+        ? 0
+        : int.parse(text.substring(match.start, match.end));
+  }
+
+  final first = intValue(firstMatch);
+  final last = intValue(lastMatch);
+
+  return first * 10 + last;
+}
+
+int documentCalibrationValue(String document) {
+  final lines = document.split('\n');
+  return lines
+      .map((element) => textCalibrationValue(element))
+      .reduce((value, element) => value + element);
 }
