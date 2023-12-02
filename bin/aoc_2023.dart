9d623be16484318a9318787bd3139d0540861335
Mickael Titeca <mickael.titeca@gmail.com>
2023-12-02 14:02:27 +0100

    Comment main method in bin folder

--- bin/aoc_2023.dart
+++ bin/aoc_2023.dart
@@ -1,5 +1,5 @@
 import 'package:aoc_2023/aoc_2023.dart' as aoc_2023;
 
 void main(List<String> arguments) {
-  print('Hello world: ${aoc_2023.calculate()}!');
+  // print('Hello world: ${aoc_2023.calculate()}!');
 }
