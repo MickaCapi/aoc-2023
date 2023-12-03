4c1032886bbfadbfcc8e7c922e98dc44b84d6b4c
Mickael Titeca <mickael.titeca@gmail.com>
2023-12-03 12:55:09 +0100

    Day 3 part 1 solution

--- lib/aoc_2023.dart
+++ lib/aoc_2023.dart
@@ -1,3 +1,44 @@
-int calculate() {
-  return 6 * 7;
+import 'dart:math';
+
+import 'package:collection/collection.dart';
+
+final numberRegExp = RegExp(r'\d+');
+final notSymbolRegExp = RegExp(r'\d|\.');
+
+int calculate(String engineSchematic) {
+  var sum = 0;
+
+  final lines = engineSchematic.split('\n');
+  final matrix = lines.map((line) => line.split('')).toList();
+
+  lines.forEachIndexed((lineIndex, line) {
+    numberRegExp.allMatches(line).forEach((match) {
+      //print('${match.group(0)} [$lineIndex - ${match.start}/${match.end}]');
+
+      final startSearchRow = max(0, lineIndex - 1);
+      final endSearchRow = min(lines.length - 1, lineIndex + 1);
+
+      final startSearchColumn = max(0, match.start - 1);
+      final endSearchColumn = min(line.length - 1, match.end);
+
+      rowLoop:
+      for (var rowIndex = startSearchRow;
+          rowIndex <= endSearchRow;
+          rowIndex++) {
+        for (var columnIndex = startSearchColumn;
+            columnIndex <= endSearchColumn;
+            columnIndex++) {
+          //print(matrix[rowIndex][columnIndex]);
+          if (notSymbolRegExp.hasMatch(matrix[rowIndex][columnIndex]) ==
+              false) {
+            //print('add');
+            sum += int.parse(match.group(0)!);
+            break rowLoop;
+          }
+        }
+      }
+    });
+  });
+
+  return sum;
 }
