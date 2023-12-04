fedda9df2e13b5b3eb4ab72ea184564475ae8bf2
Mickael Titeca <mickael.titeca@gmail.com>
2023-12-04 08:54:18 +0100

    Day 4 part 1 solution

--- lib/aoc_2023.dart
+++ lib/aoc_2023.dart
@@ -1,3 +1,39 @@
-int calculate() {
-  return 6 * 7;
+/*
+Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11'
+*/
+int calculate(String cards) {
+  return cards.split('\n').map((card) {
+    final pipeSplit = card.split(':')[1].split('|');
+    final winningNumbers = pipeSplit[0]
+        .split(' ')
+        .map((element) => int.tryParse(element))
+        .where((element) => element != null)
+        .toSet();
+    final numbers = pipeSplit[1]
+        .split(' ')
+        .map((element) => int.tryParse(element))
+        .where((element) => element != null)
+        .toSet();
+    final intersectLength = winningNumbers.intersection(numbers).length;
+    var value = 0;
+    for (var i = 0; i < intersectLength; i++) {
+      if (i == 0) {
+        value = 1;
+      } else {
+        value *= 2;
+      }
+    }
+    return value;
+  }).reduce((value, element) => value + element);
+
+  // return 13;
 }
+
+// 6:30 7:50
+// 7:00 8:10
+// 7:30 22:00
