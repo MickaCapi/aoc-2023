5dac33715bde3b0f75c3ff3835616ecc890e5151
Mickael Titeca <mickael.titeca@gmail.com>
2023-12-06 11:11:59 +0100

    Day 6 part 1 solution

--- lib/aoc_2023.dart
+++ lib/aoc_2023.dart
@@ -1,6 +1,32 @@
 /*
-
+Time:      7  15   30
+Distance:  9  40  200
 */
 int calculate(String input) {
-  return 6 * 7;
+  final timeDistanceLists = input
+      .split('\n')
+      .map(
+        (e) => RegExp(r'\d+')
+            .allMatches(e)
+            .map((e) => int.parse(e.group(0)!))
+            .toList(),
+      )
+      .toList();
+
+  final winList = <int>[];
+  for (var i = 0; i < timeDistanceLists[0].length; i++) {
+    final time = timeDistanceLists[0][i];
+    final distance = timeDistanceLists[1][i];
+
+    var wins = 0;
+    for (var j = 1; j < time; j++) {
+      //print('${(time - j) * j} > $distance');
+      if ((time - j) * j > distance) {
+        wins++;
+      }
+    }
+    winList.add(wins);
+  }
+
+  return winList.reduce((value, element) => value * element);
 }
--- test/aoc_2023_test.dart
+++ test/aoc_2023_test.dart
@@ -3,15 +3,17 @@ import 'package:test/test.dart';
 
 void main() {
   test('calculate', () {
-    final input = '''''';
+    final input = '''Time:      7  15   30
+Distance:  9  40  200''';
 
     print(calculate(input));
 
-    expect(calculate(input), 24000);
+    // expect(calculate(input), 24000);
   });
 
   test('aoc 2023', () {
-    final input = '''''';
+    final input = '''Time:        38     67     76     73
+Distance:   234   1027   1157   1236''';
 
     print(calculate(input));
   });
