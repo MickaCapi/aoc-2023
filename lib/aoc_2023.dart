d3a74f329bb3029f0078f8a01ffd2f4704d002ab
Mickael Titeca <mickael.titeca@gmail.com>
2023-12-08 20:37:49 +0100

    Day 8 part 2 solution

--- lib/aoc_2023.dart
+++ lib/aoc_2023.dart
@@ -9,37 +9,48 @@ EEE = (EEE, EEE)
 GGG = (GGG, GGG)
 ZZZ = (ZZZ, ZZZ)
 */
+
 int calculate(String input) {
   final instructionsElements = input.split('\n\n');
   final instructions = instructionsElements[0].split('');
   final nodes = instructionsElements[1];
 
-  var position = 'AAA';
-  var steps = 0;
-  while (position != 'ZZZ') {
-    final instruction = instructions[steps % instructions.length];
+  var positions = RegExp('^\\w{2}A', multiLine: true)
+      .allMatches(nodes)
+      .map((e) => e.group(0)!)
+      .toList();
+  var steps = <String, int>{};
+  var step = 0;
+  while (steps.length < positions.length) {
+    final instruction = instructions[step % instructions.length];
 
-    final node =
-        RegExp('$position = \\(\\w{3}, \\w{3}\\)').firstMatch(nodes)!.group(0)!;
-    final positionDirections = node.split(' = ');
-    final directions =
-        positionDirections[1].replaceAll(RegExp(r'\(|\)'), '').split(', ');
-    final left = directions[0];
-    final right = directions[1];
+    for (var i = 0; i < positions.length; i++) {
+      final position = positions[i];
 
-    if (instruction == 'L') {
-      position = left;
-    } else {
-      position = right;
+      final node = RegExp('$position = \\(\\w{3}, \\w{3}\\)')
+          .firstMatch(nodes)!
+          .group(0)!;
+      final positionDirections = node.split(' = ');
+      final directions =
+          positionDirections[1].replaceAll(RegExp(r'\(|\)'), '').split(', ');
+      final left = directions[0];
+      final right = directions[1];
+
+      if (instruction == 'L') {
+        positions[i] = left;
+      } else {
+        positions[i] = right;
+      }
+
+      if (positions[i].endsWith('Z') && !steps.containsKey(position)) {
+        steps[positions[i]] = step + 1;
+      }
     }
-    steps++;
-
-    if (position == 'ZZZ') {
-      break;
-    }
+    step++;
   }
 
-  return steps;
+  return steps.values
+      .reduce((value, element) => (value * element) ~/ value.gcd(element));
 }
 
 class Node {
--- test/aoc_2023_test.dart
+++ test/aoc_2023_test.dart
@@ -21,9 +21,29 @@ ZZZ = (ZZZ, ZZZ)''';
   test('calculate2', () {
     final input = '''LLR
 
+DDA = (DDZ, DDZ)
+DDB = (DDA, DDZ)
+DDZ = (DDZ, DDZ)
 AAA = (BBB, BBB)
 BBB = (AAA, ZZZ)
 ZZZ = (ZZZ, ZZZ)''';
+
+    print(calculate(input));
+
+    // expect(calculate(input), 24000);
+  });
+
+  test('calculate3', () {
+    final input = '''LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)''';
 
     print(calculate(input));
 
