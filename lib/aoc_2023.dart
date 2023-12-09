40c881ebdea0d4a21b174195f7e39fabe6d9a423
Mickael Titeca <mickael.titeca@gmail.com>
2023-12-09 15:13:12 +0100

    Day 9 part 1 solution

--- lib/aoc_2023.dart
+++ lib/aoc_2023.dart
@@ -1,6 +1,36 @@
 /*
-
+0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45
 */
 int calculate(String input) {
-  return 6 * 7;
+  return input.split('\n').map((e) {
+    return RegExp(r'-*\d+')
+        .allMatches(e)
+        .map((e) => int.parse(e.group(0)!))
+        .toList();
+  }).map((e) {
+    return diffLists(e)
+        .fold(0, (previousValue, element) => previousValue + element.last);
+  }).reduce((value, element) => value + element);
+}
+
+List<List<int>> diffLists(List<int> list) {
+  final lists = <List<int>>[list];
+
+  var currentDiffList = diffList(list);
+  while (!currentDiffList.every((element) => element == 0)) {
+    lists.add(currentDiffList);
+    currentDiffList = diffList(currentDiffList);
+  }
+  lists.add(currentDiffList);
+  return lists;
+}
+
+List<int> diffList(List<int> list) {
+  final diffList = <int>[];
+  for (var i = 0; i < list.length - 1; i++) {
+    diffList.add(list[i + 1] - list[i]);
+  }
+  return diffList;
 }
