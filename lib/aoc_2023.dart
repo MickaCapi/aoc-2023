c3a5c0ed8e324111e9be9d097182adc49586b264
Mickael Titeca <mickael.titeca@gmail.com>
2023-12-12 17:56:43 +0100

    Day 12 part 2 solution

--- lib/aoc_2023.dart
+++ lib/aoc_2023.dart
@@ -1,50 +1,78 @@
-import 'dart:math';
-
 int calculate(String input) {
-  return input
-      .split('\n')
-      .map((e) => arrangementsCount(e))
-      .reduce((value, element) => value + element);
-  //return 21;
+  return input.split('\n').map((e) {
+    final statesDamageCounts = e.split(' ');
+    final states = formatStates(statesDamageCounts[0]);
+    final damageCounts = formatDamageCounts(statesDamageCounts[1])
+        .split(',')
+        .map((e) => int.parse(e))
+        .toList();
+    return arrangementsCount(states, damageCounts,
+        damageCounts.reduce((value, element) => value + element + 1));
+  }).reduce((value, element) => value + element);
 }
 
-int arrangementsCount(String record) {
-  final statesDamageCounts = record.split(' ');
-  final states = statesDamageCounts[0];
-  final damageCounts =
-      statesDamageCounts[1].split(',').map((e) => int.parse(e)).toList();
+// https://en.wikipedia.org/wiki/Memoization
+// key = states + damageCounts
+final d = <String, int>{};
+
+int arrangementsCount(String states, List<int> damageCounts, int totalDamage) {
+  if (d.containsKey(states + damageCounts.toString())) {
+    return d[states + damageCounts.toString()]!;
+  }
 
   var count = 0;
   final firstDamage = damageCounts[0];
   var hashFound = false;
   for (var i = 0; i < states.length; i++) {
-    // print('$i - $record');
-    if (i + firstDamage > states.length) {
+    final iPlusDamage = i + firstDamage;
+    if (totalDamage > states.length - i) {
       break;
     }
-    final stateSubstring = states.substring(i, i + firstDamage);
-    if (hashFound && stateSubstring[0] != '#') {
+    final stateSubstring = states.substring(i, iPlusDamage);
+    final firstCharacter = stateSubstring[0];
+    if (hashFound && firstCharacter != '#') {
       break;
     }
-    hashFound = stateSubstring[0].contains('#');
-    if (!stateSubstring.replaceAll('?', '#').contains('.') &&
-        (states.length == i + firstDamage || states[i + firstDamage] != '#') &&
+    hashFound = firstCharacter == '#';
+    if (!stateSubstring.contains('.') &&
+        (states.length == iPlusDamage || states[iPlusDamage] != '#') &&
         (i == 0 || states[i - 1] != '#')) {
       if (damageCounts.length == 1) {
-        if (states.substring(i + firstDamage).contains('#')) {
+        if (states.substring(iPlusDamage).contains('#')) {
           continue;
         }
-        // print('count + 1');
         count += 1;
       } else {
         if (i + firstDamage + 1 > states.length) {
-          continue;
+          break;
         }
         count += arrangementsCount(
-            '${states.substring(i + firstDamage + (stateSubstring != '.' ? 1 : 0))} ${damageCounts.sublist(1).join(',')}');
+          states.substring(i + firstDamage + (stateSubstring != '.' ? 1 : 0)),
+          damageCounts.sublist(1),
+          totalDamage - firstDamage - 1,
+        );
       }
     }
   }
 
+  d.addAll({states + damageCounts.toString(): count});
+
   return count;
+}
+
+String formatRecord(String e) {
+  final statesDamageCounts = e.split(' ');
+  final states = formatStates(statesDamageCounts[0]);
+  final damageCounts = formatDamageCounts(statesDamageCounts[1]);
+  return '$states $damageCounts';
+}
+
+final multiply = 5;
+
+String formatStates(String states) {
+  return Iterable.generate(multiply, (_) => states).join('?');
+}
+
+String formatDamageCounts(String damageCounts) {
+  return Iterable.generate(multiply, (_) => damageCounts).join(',');
 }
