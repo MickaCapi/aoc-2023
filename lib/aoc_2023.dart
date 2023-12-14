38c47f6bb181b530ffd3cbd45850bc2475c25d89
Mickael Titeca <mickael.titeca@gmail.com>
2023-12-14 16:19:21 +0100

    Day 14 part 1 solution

--- lib/aoc_2023.dart
+++ lib/aoc_2023.dart
@@ -2,5 +2,35 @@
 
 */
 int calculate(String input) {
-  return 6 * 7;
+  final places = tiltToNorth(
+    input.split('\n').map((e) => e.split('').toList()).toList(),
+  );
+
+  var value = 0;
+  for (var i = 0; i < places.length; i++) {
+    final rowValue = places.length - i;
+    value += places[i].where((e) => e == 'O').length * rowValue;
+  }
+  return value;
+}
+
+List<List<String>> tiltToNorth(List<List<String>> currentPlaces) {
+  // i = vertical
+  // j = horizontal
+  final places = currentPlaces.toList();
+  for (var i = 0; i < places.length; i++) {
+    final placesLine = places[i];
+    for (var j = 0; j < placesLine.length; j++) {
+      final place = placesLine[j];
+      var previousLineIndex = i - 1;
+      if (i > 0 && place == 'O' && places[previousLineIndex][j] == '.') {
+        while (previousLineIndex >= 0 && places[previousLineIndex][j] == '.') {
+          previousLineIndex--;
+        }
+        places[previousLineIndex + 1][j] = 'O';
+        places[i][j] = '.';
+      }
+    }
+  }
+  return places;
 }
