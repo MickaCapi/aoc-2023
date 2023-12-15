190eca2daf1e65ccc1079ffd5435e2f11f8622e9
Mickael Titeca <mickael.titeca@gmail.com>
2023-12-15 09:12:19 +0100

    Day 15 part 1 solution

--- lib/aoc_2023.dart
+++ lib/aoc_2023.dart
@@ -2,5 +2,21 @@
 
 */
 int calculate(String input) {
-  return 6 * 7;
+  return input.split(',').map((e) => hashValue(e)).reduce((v, e) => v + e);
+}
+
+/*
+Determine the ASCII code for the current character of the string.
+Increase the current value by the ASCII code you just determined.
+Set the current value to itself multiplied by 17.
+Set the current value to the remainder of dividing itself by 256.
+*/
+int hashValue(String input) {
+  var hashValue = 0;
+  for (var codeUnit in input.codeUnits) {
+    hashValue += codeUnit;
+    hashValue *= 17;
+    hashValue %= 256;
+  }
+  return hashValue;
 }
